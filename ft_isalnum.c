9d4f67332b348d889345afec53c205cdfe8e6a4c
bouabbadi abdessamad <bouabbadiabdessamad@gmail.com>
2024-12-31 19:23:35 +0100

    Update ft_isalnum.c

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -11,7 +11,7 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-
+  
 int	ft_isalnum(int ch)
 {
 	return ((ft_isalpha(ch)) || (ft_isdigit(ch)));
